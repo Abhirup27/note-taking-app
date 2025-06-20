39a4c9fe64d1390809a8c7fc400761a84a686fca
Abhirup Bhattacharyya <abhirup27022001@outlook.com>
2025-06-20 17:04:59 +0530

    initial express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Happy developing ✨')
+import express = require('express');
+import {urlencoded} from "express";
+import cookieParser from "cookie-parser";
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: false }));
+ app.listen(3000, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
+});
+app.init();
+// const csrf = async(req: Request, res: Response, next) => {
+//
+// }
+// app.use('/dashboard', csrf);
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+
+
